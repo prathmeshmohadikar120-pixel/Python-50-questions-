74d0770d2acda3b1131f57a43f6f701bec39d7f6
Prathmesh Mohadikar <prathmeshmohadikar120@gmail.com>
2026-09-24 11:39:28 +0530

    Update 19. Simple calculator using if,elif statements.py

--- 19. Simple calculator using if,elif statements.py
+++ 19. Simple calculator using if,elif statements.py
@@ -1,29 +1,14 @@
-#first method
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = input("Enter operator here : ")
 
-a = float(input("Enter the first number: "))
-b = float(input("Enter the second number: "))
-operator = input("Enter an operator : ")
-
-if operator == '+':
-    result = a + b
-    print(f"{a} + {b} = {result}")
-elif operator == '-':
-    result = a - b
-    print(f"{a} - {b} = {result}")
-elif operator == '*':
-    result = a * b
-    print(f"{a} * {b} = {result}")
-elif operator == '/':
-    result = a / b
-    print(f"{a} / {b} = {result}")
-
+if c == '+':
+    print(a + b)
+elif c == '-':
+    print(a - b)
+elif c == '*':
+    print(a * b)
+elif c == '/':
+    print(a / b)
 else:
-    print("wrong input / invlid")
-#second method
-a = float(input("Enter the first number: "))
-b = float(input("Enter the second number: "))
-operator = input("Enter an operator: ")
-
-result = eval(f"{a}{operator}{b}")
-
-print(f"{a} {operator} {b} = {result}")
+    print("Invalid inputs,,")
